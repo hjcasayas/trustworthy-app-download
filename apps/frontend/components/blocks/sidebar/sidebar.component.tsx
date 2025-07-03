3dfc13739ed12280dda2261e54be067a56a5b2a7
hjcasayas <henlyjade.casayas@gmail.com>
2025-07-03 04:35:47 +0000

    style(fe): updated padding bottom for tablet and desktop

--- apps/frontend/components/blocks/sidebar/sidebar.component.tsx
+++ apps/frontend/components/blocks/sidebar/sidebar.component.tsx
@@ -17,7 +17,7 @@ export const SidebarBlockComponent = ({
 }: SidebarBlock) => {
   return (
     <aside className="bg-background-1 desktop:w-2/5 desktop:items-end flex flex-col">
-      <div className="tablet:px-10 desktop:px-0 desktop:pt-[1.875rem] tablet:gap-y-[3.125rem] desktop:w-[22.1875rem] desktop:ml-10 desktop:mr-[5.3125rem] flex w-full flex-col gap-y-10 self-end px-5 pb-[1.875rem] pt-5">
+      <div className="tablet:px-10 desktop:px-0 desktop:pt-[1.875rem] tablet:gap-y-[3.125rem] desktop:w-[22.1875rem] desktop:ml-10 desktop:mr-[5.3125rem] tablet:pb-5 desktop:pb-[1.875rem] flex w-full flex-col gap-y-10 self-end px-5 pb-[1.875rem] pt-5">
         <SidebarNavComponent {...{ website, cta }} />
         <SidebarHeadlineComponent
           {...{ headline1, headline2, headline3, description }}

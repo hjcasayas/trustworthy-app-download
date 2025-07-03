7e8cd199b9d754facbc2f1da48c9d123270f0c3b
hjcasayas <henlyjade.casayas@gmail.com>
2025-07-03 08:45:08 +0000

    feat(fe): added logic for isExternal property

--- apps/frontend/components/common/cta/cta.component.tsx
+++ apps/frontend/components/common/cta/cta.component.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import { CommonLink } from "../link/link.interface";
 
-export const CommonCtaComponent = ({ label, url }: CommonLink) => {
+export const CommonCtaComponent = ({ label, url, isExternal }: CommonLink) => {
   return (
     <Link
       className="paragraph text-paragraph-1 bg-accent-1 px-[0.875rem] py-[0.6875rem]"
       href={url}
+      target={isExternal ? "_blank" : "_self"}
+      rel={isExternal ? "noopener noreferrer" : undefined}
     >
       {label}
     </Link>
